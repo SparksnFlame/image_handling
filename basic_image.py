8a55da3683d855e7ec46ef9e5e2c05e58aa523ab
SparksnFlame <SparksnFlame@users.noreply.github.com>
2025-03-18 23:47:56 +0000

    First test

--- basic_image.py
+++ basic_image.py
@@ -2,4 +2,14 @@
 # Relevant Python libraries
 # Python Image Library (PIL)
 
+import os
+import sys
 from PIL import Image
+
+test_image = Image.open("seabeach.jpg")
+
+print(test_image.format, test_image.size, test_image.mode)
+test_image.show()
+convertim2 = test_image
+convertim2 = Image.open("seabeach.jpg").convert("L")
+convertim2.show()
